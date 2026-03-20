dac5f1b0ce57945f066e101cb6079de63c20fbd5
Klint Ruales <klintruales11@gmail.com>
2026-03-20 22:26:37 +0800

    add protected routes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,13 +1,31 @@
 import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import API from "../api";
 
 const ProtectedRoute = ({ children }) => {
-    const token = localStorage.getItem("token");
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    if(!token){
-        return <Navigate to="/" />
-    }
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await API.get("/api/user");
+        setUser(res.data);
+      } catch (err) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    return children;
-}
+    checkAuth();
+  }, []);
+
+  if (loading) return <p>Checking authentication...</p>;
+
+  if (!user) return <Navigate to="/" />;
+
+  return children;
+};
 
 export default ProtectedRoute;
